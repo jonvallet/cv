c71d93c8104f655965a0bbe5239e4d52eaf9d08d
Jon Vallet <j.vallet@gmail.com>
2026-06-12 12:46:54 +0100

    no sandbox

--- convert-to-pdf.sh
+++ convert-to-pdf.sh
@@ -2,4 +2,4 @@
 # Requires Node.js and md-to-pdf
 # npm install -g md-to-pdf
 
-md-to-pdf README.md --stylesheet cv-print.css && mv README.pdf Jon_Vallet_CV.pdf
+md-to-pdf README.md --stylesheet cv-print.css --launch-options '{"args": ["--no-sandbox"]}' && mv README.pdf Jon_Vallet_CV.pdf
